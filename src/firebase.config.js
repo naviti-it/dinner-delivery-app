ad653d8fbe03673ba7fa377029cd64ab7b158905
naviti-it <naviti.it@gmail.com>
2022-11-28 06:58:32 +0300

    edit firebase webpack config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,25 +3,25 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCQQy0Lvcwpxn74y7D6P4XfLFZQXXoTRQc",
-  authDomain: "mystore-f00.firebaseapp.com",
-  databaseURL: "https://mystore-f00-default-rtdb.firebaseio.com",
-  projectId: "mystore-f00",
-  storageBucket: "mystore-f00.appspot.com",
-  messagingSenderId: "496111507537",
-  appId: "1:496111507537:web:d1c8a5439d653b71370070",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQQy0Lvcwpxn74y7D6P4XfLFZQXXoTRQc",
+//   authDomain: "mystore-f00.firebaseapp.com",
+//   databaseURL: "https://mystore-f00-default-rtdb.firebaseio.com",
+//   projectId: "mystore-f00",
+//   storageBucket: "mystore-f00.appspot.com",
+//   messagingSenderId: "496111507537",
+//   appId: "1:496111507537:web:d1c8a5439d653b71370070",
+// };
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const ESLintWebpackPlugin = require('eslint-webpack-plugin')
+const dotenv = require('dotenv')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -27,6 +28,9 @@ const plugins = [
     }),
     new MiniCssExtractPlugin({
         filename: filename('css')
+    }),
+    new dotenv.config({
+        path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
     }),
 ]
 
